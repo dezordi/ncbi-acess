6e0ab005c92e9364a72334b85421f8b94a518ccc
Yago <yag.dias@gmail.com>
2021-03-16 19:11:03 -0300

    I change Digite for Type, so is in the same language

--- ncbi_seq_retrieve.py
+++ ncbi_seq_retrieve.py
@@ -49,7 +49,7 @@ if entrez_database == 'protein' and any(x in out_type for x in protein_negative_
 if 'fasta_cds' in out_type and text_format == 'xml':
     sys.exit("ERROR: Invalid options of text format for output type")
 
-Entrez.email = str(input('Digite your e-mail to access NCBI:'))
+Entrez.email = str(input('Type your e-mail to access NCBI:'))
 
 def efetch_function(var_data,var_id,var_rettype,var_retmode):
     """
